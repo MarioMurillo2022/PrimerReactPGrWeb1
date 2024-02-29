89cbcb947701f829428b6ab10f55ceca3caeac3c
MarioMurillo2022 <Mariomun17@gmail.com>
2024-02-29 15:25:23 -0600

    Commit Card Emma
    Se creo el componente de testimonio

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import '../src/style-sheets/Testimonio.css'
+import Testimonio from './components/Testimonio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className='contenedor-principal'>
+          <h1>Esto es lo que dicen los estudiantes de CEUTEC</h1>
+          <Testimonio
+            imagen='Emma.png'
+            nombre='Emma Bostian'
+            pais='Suecia'
+            cargo='Ingeneria de Software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero la clase de programación web I fue donde aprendí. Estudiar JavaScript, así como estructuras de datos y algoritmos en la clase me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+          />
+        </div>
+
+      </div>
+    </>
+
   );
 }
 
